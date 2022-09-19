f38ef2947401a3cb052cdfd7c6d31b3aae47fcef
alinakuzina <kuzina.lg.4.1@gmail.com>
2022-09-19 20:26:36 +0200

    designing  cart dropdown items

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,12 +1,18 @@
 import "./CartItem.scss";
 
 const CartItem = ({ cartItem }) => {
-  const { name, quantity, selectedSize } = cartItem;
+  const { name, quantity, selectedSize, images, price } = cartItem;
+  console.log(images);
   return (
-    <div>
-      <h2>{name}</h2>
-      <span>{quantity}</span>
-      <span>{selectedSize}</span>
+    <div className="cart-container">
+      <img src={images[0].url} className="img-card-small" />
+      <div className="info-cart-container">
+        <h2>{name}</h2>
+        <div className="cart-item-details">
+          {quantity} items &times; {price.formattedValue}
+        </div>
+        <div className="cart-item-details">Size: {selectedSize}</div>
+      </div>
     </div>
   );
 };
