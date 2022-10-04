f17e7aa2f22e106778f6eb1b7fc4be1506e82868
alinakuzina <kuzina.lg.4.1@gmail.com>
2022-10-04 11:09:45 +0200

    make separate ShopPage in components instead of Shop route

---
+++ src/components/ShopPage/ShopPage.jsx
@@ -0,0 +1,44 @@
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { ProductContext } from "../../context/ProductsContext";
+import ProductCard from "../../components/ProductCard/ProductCard";
+import "./ShopPage.scss";
+import { UserContext } from "../../context/UserContext";
+
+const ShopPage = ({ url }) => {
+  const { products } = useContext(ProductContext);
+  const { categories } = useContext(UserContext);
+  let navigate = useNavigate();
+  let redirectToPage = (e) => {
+    let link = e.target.id;
+    navigate(`/${link}`);
+  };
+
+  let currentCategory = `${url.split("_")[0]}_all`;
+  return (
+    <div className="products-page-container">
+      <div className="subcategories">
+        {categories
+          .filter((el) => el.tagCode === currentCategory)[0]
+          ?.subCategories.map((category) => (
+            <div
+              className="link-sub-category"
+              id={category.tagCode}
+              onClick={redirectToPage}
+              key={category.tagCode + Math.random()}
+            >
+              {category.catName}
+            </div>
+          ))}
+      </div>
+
+      <div className="products-container">
+        {products.map((product) => (
+          <ProductCard key={product.code} product={product}></ProductCard>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ShopPage;
--- src/routes/Shop/Shop.jsx
+++ src/routes/Shop/Shop.jsx
@@ -1,44 +1,6 @@
-import React, { useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import { ProductContext } from "../../context/ProductsContext";
-import ProductCard from "../../components/ProductCard/ProductCard";
-import "./Shop.scss";
-import { UserContext } from "../../context/UserContext";
-
+import ShopPage from "../../components/ShopPage/ShopPage";
 const Shop = ({ url }) => {
-  const { products } = useContext(ProductContext);
-  const { categories } = useContext(UserContext);
-  let navigate = useNavigate();
-  let redirectToPage = (e) => {
-    let link = e.target.id;
-    navigate(`/${link}`);
-  };
-
-  let currentCategory = `${url.split("_")[0]}_all`;
-  return (
-    <div className="products-page-container">
-      <div className="subcategories">
-        {categories
-          .filter((el) => el.tagCode === currentCategory)[0]
-          ?.subCategories.map((category) => (
-            <div
-              className="link-sub-category"
-              id={category.tagCode}
-              onClick={redirectToPage}
-              key={category.tagCode + Math.random()}
-            >
-              {category.catName}
-            </div>
-          ))}
-      </div>
-
-      <div className="products-container">
-        {products.map((product) => (
-          <ProductCard key={product.code} product={product}></ProductCard>
-        ))}
-      </div>
-    </div>
-  );
+  return <ShopPage url={url} />;
 };
 
 export default Shop;
