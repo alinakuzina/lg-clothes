e8dc7d42601361b7c42815c52945e895c43a81cb
alinakuzina <kuzina.lg.4.1@gmail.com>
2022-10-05 15:24:57 +0200

    switching navigation to mobile when height is less then 50 en and wisth less then 53em

--- src/components/Navigation/NavLink.jsx
+++ src/components/Navigation/NavLink.jsx
@@ -1,19 +1,26 @@
 import { Link } from "react-router-dom";
 import style from "./NavLink.module.scss";
+import { useState } from "react";
 
 let NavLink = (props) => {
+  const [isOpenDropdown, setIsOpenDropdown] = useState(false);
+  const closeDropdownHandler = () => {
+    // setIsOpenDropdown((prev) => !prev);
+  };
+
   return (
     <div className={style.dropdown}>
       <Link
         className={style.nav_link}
         to={`${props.category.tagCode}`}
+        onClick={closeDropdownHandler}
         id={`${props.category.tagCode}`}
         key={`${props.tagCode}${Math.random()}`}
       >
         {props.category.catName}
       </Link>
       {props.category.subCategories.length > 0 && (
-        <div className={style.dropdown_content}>
+        <div className={style.dropdown_content} onClick={closeDropdownHandler}>
           {props.category.subCategories.map((el) => {
             return (
               <Link
