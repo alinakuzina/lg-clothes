df3b836323e12200ad7948a1b9b4a9419c53ed24
alinakuzina <kuzina.lg.4.1@gmail.com>
2022-10-19 17:31:46 +0200

    scroll to top in favorite page

--- src/components/FavoritesPage/FavoritesPage.jsx
+++ src/components/FavoritesPage/FavoritesPage.jsx
@@ -2,9 +2,14 @@ import { useSelector } from "react-redux";
 import style from "./FavoritesPage.module.scss";
 import { selectFavoritesProducts } from "../../store/Products/ProductsSelector";
 import ProductCard from "../ProductCard/ProductCard";
+import { useEffect } from "react";
 
 const FavoritesPage = () => {
   const favorites = useSelector(selectFavoritesProducts);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div className={style.favorite_page_container}>
       <div className={style.products_container}>
